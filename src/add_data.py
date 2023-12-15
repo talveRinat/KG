087c5d29424d7eec8fa66f1a07bba641ee41bc05
Dmitry Nesmeyanov <miradg99@gmail.com>
2023-12-15 18:59:21 +0400

    fix

--- src/add_data.py
+++ src/add_data.py
@@ -9,9 +9,10 @@ from .custom_graph import CustomGraph
 
 class AddData:
     @abstractmethod
-    def add_stadium(
-        g: CustomGraph, country_name: object, city_name: object, stadium_name: object
-    ):
+    def add_stadium(g: CustomGraph,
+                    country_name: object,
+                    city_name: object,
+                    stadium_name: object):
         try:
             g.add_cls_instance(country_name, "Countries")
             g.add_cls_instance(city_name, "Cities")
@@ -24,7 +25,12 @@ class AddData:
             print(e)
 
     @abstractmethod
-    def add_dates(g: CustomGraph, instance_name: object, start_date: object, end_date: object):
+    def add_dates(
+        g: CustomGraph,
+        instance_name: object,
+        start_date: Optional[object] = None,
+        end_date: Optional[object] = None,
+    ):
         try:
             g.add_cls_instance(instance_name, "Dates")
             if start_date is not None:
@@ -39,18 +45,25 @@ class AddData:
                     "EndDate",
                     Literal(end_date, datatype=XSD.dateTime),
                 )
-            g.add_obj_prop_instance(instance_name, "onDate", instance_name + "_date")
+            g.add_obj_prop_instance(instance_name,
+                                    "onDate",
+                                    instance_name + "_date")
             print("add dates success")
         except Exception as e:
             print(e)
 
     @abstractmethod
-    def add_statistic(g: CustomGraph, instance_name: object, statistic: Optional[object]):
+    def add_statistic(g: CustomGraph,
+                      instance_name: object,
+                      statistic: Optional[object] = None):
         try:
             if statistic is not None:
-                g.add_cls_instance(instance_name + "_statistic", "StatisticsCls")
+                g.add_cls_instance(instance_name + "_statistic",
+                                   "StatisticsCls")
                 g.add_data_prop_instance(
-                    instance_name + "_statistic", "StatisticsCls", Literal(statistic)
+                    instance_name + "_statistic",
+                    "StatisticsCls",
+                    Literal(statistic)
                 )
                 g.add_obj_prop_instance(
                     instance_name, "hasStatistic", instance_name + "_statistic"
@@ -64,9 +77,9 @@ class AddData:
         g: CustomGraph,
         competition_type: object,
         competition_name: object,
-        competition_start_date: Optional[object],
-        compettiton_end_date: Optional[object],
-        competition_statistic: Optional[object],
+        competition_start_date: Optional[object] = None,
+        compettiton_end_date: Optional[object] = None,
+        competition_statistic: Optional[object] = None,
     ):
         """
         Competition type in ["local", "national"]
@@ -80,9 +93,10 @@ class AddData:
                 g.add_cls_instance(competition_name, "LocalCompetitions")
             else:
                 g.add_cls_instance(competition_name, "NationalCompetitions")
-            AddData.add_dates(
-                competition_name, competition_start_date, compettiton_end_date
-            )
+            AddData.add_dates(g,
+                              competition_name,
+                              competition_start_date,
+                              compettiton_end_date)
             AddData.add_statistic(competition_name, competition_statistic)
             print("Success")
         except Exception as e:
@@ -93,9 +107,9 @@ class AddData:
         g: CustomGraph,
         team_type: object,
         team_name: object,
-        team_found_date: Optional[object],
-        team_close_date: Optional[object],
-        team_statistic: Optional[object],
+        team_found_date: Optional[object] = None,
+        team_close_date: Optional[object] = None,
+        team_statistic: Optional[object] = None,
     ):
         """
         team_type in ["NationalTeams", "SockerClubs"]
@@ -109,14 +123,15 @@ class AddData:
                 g.add_cls_instance(team_name, "NationalTeams")
             else:
                 g.add_cls_instance(team_name, "SockerClubs")
-            AddData.add_dates(team_name, team_found_date, team_close_date)
+            AddData.add_dates(g,team_name, team_found_date, team_close_date)
             AddData.add_statistic(team_name, team_statistic)
             print("Success")
         except Exception as e:
             print(e)
 
     @abstractmethod
-    def add_player_position(g: CustomGraph, position_name: Optional[object]):
+    def add_player_position(g: CustomGraph,
+                            position_name: Optional[object] = None):
         try:
             if position_name is not None:
                 g.add_cls_instance(position_name, "Position")
@@ -132,16 +147,18 @@ class AddData:
         g: CustomGraph,
         player_name: object,
         team_name: object,
-        position_name: Optional[object],
-        player_emploing_date: Optional[object],
-        player_fire_date: Optional[object],
-        player_statistic: Optional[object],
+        position_name: Optional[object] = None,
+        player_emploing_date: Optional[object] = None,
+        player_fire_date: Optional[object] = None,
+        player_statistic: Optional[object] = None,
     ):
         try:
             g.add_cls_instance(player_name, "Players")
             g.add_obj_prop_instance(player_name, "onPosition", position_name)
-            AddData.add_dates(player_name, player_emploing_date, player_fire_date)
-            AddData.add_statistic(player_name, team_statistic)
+            AddData.add_dates(g,player_name,
+                              player_emploing_date,
+                              player_fire_date)
+            AddData.add_statistic(player_name, player_statistic)
             print("Success")
         except Exception as e:
             print(e)
@@ -151,14 +168,15 @@ class AddData:
         g: CustomGraph,
         coach_name: object,
         team_name: object,
-        coach_emploing_date: Optional[object],
-        coach_fire_date: Optional[object],
-        coach_statistic: Optional[object],
+        coach_emploing_date: Optional[object] = None,
+        coach_fire_date: Optional[object] = None,
+        coach_statistic: Optional[object] = None,
     ):
         try:
             g.add_cls_instance(coach_name, "Coaches")
-            AddData.add_dates(coach_name, coach_emploing_date, coach_fire_date)
-            AddData.add_statistic(coach_name, team_statistic)
+            AddData.add_dates(g,coach_name, coach_emploing_date, coach_fire_date)
+            AddData.add_statistic(coach_name,
+                                  coach_statistic)
             print("Success")
         except Exception as e:
             print(e)
@@ -170,14 +188,16 @@ class AddData:
         competition_name: object,
         team1_name: object,
         team2_name: object,
-        play_date: Optional[object],
-        play_statistic: Optional[object],
+        play_date: Optional[object] = None,
+        play_statistic: Optional[object] = None,
     ):
         try:
             g.add_cls_instance(play_name, "Play")
-            g.add_obj_prop_instance(play_name, "isPartOfCompetition", competition_name)
+            g.add_obj_prop_instance(play_name,
+                                    "isPartOfCompetition",
+                                    competition_name)
             g.add_obj_prop_instance(team1_name, "playAgainst", team2_name)
-            AddData.add_dates(play_name, play_date, None)
+            AddData.add_dates(g,play_name, play_date, None)
             AddData.add_statistic(play_name, play_statistic)
             print("Success")
         except Exception as e:
